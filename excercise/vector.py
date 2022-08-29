a9eff70cf815a240b5f53a94900ab7f17e24756c
chankxow <101338054+chankxow@users.noreply.github.com>
2022-08-29 21:29:45 +0700

    1.1

--- excercise/vector.py
+++ excercise/vector.py
@@ -1,6 +1,6 @@
-v1,v2,v3 = [float(e) for e in  input("พิมจำนวน v1 v2 v3 โดยเว้นวรรคแต่ละตัว : ").split()]
-u1,u2,u3 = [float(e) for e in input("พิมจำนวน u1 u2 u3 โดยเว้นวรรคแต่ละตัว :").split()]
+v1,v2,v3 = [float(e) for e in  input("พิมจำนวน v1 v2 v3 โดยเว้นวรรคแต่ละตัว : ").split()]   #กำหนดตัวแปร v 
+u1,u2,u3 = [float(e) for e in input("พิมจำนวน u1 u2 u3 โดยเว้นวรรคแต่ละตัว :").split()]     #กำหนดตัวแปร u
 
-dot_product = v1*u1 + v2*u2 + v3*u3 
+dot_product = v1*u1 + v2*u2 + v3*u3 #กำหนดสูตรการหาค่า
 
-print(f"ค่า dot product ที่ได้: {dot_product}")
+print(f"ค่า dot product ที่ได้: {dot_product}") #output ข้อความ
